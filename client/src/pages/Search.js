3f4a9a9bf20d7f7c359a508c7edfca76ea44986f
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-10 09:25:56 -0500

    book information is rendering to the page when searching for a specific book

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,15 +3,49 @@ import API from '../utils/API';
 
 function Search() {
   const [books, setBooks] = useState([]);
-  const getbooks = () => {
-    API.searchBooks('harry potter').then((response) => {
-      console.log(response);
+  const [bookSearch, setBookSearch] = useState('');
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    console.log(value);
+    setBookSearch(value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    API.searchBooks(bookSearch).then((response) => {
+      console.log(response.data.items);
+      setBooks(response.data.items);
     });
   };
-  getbooks();
   return (
     <div>
       <h1>search page</h1>
+      <form id="searchForm">
+        <input id="seach" onChange={handleInputChange}></input>
+        <button type="submit" onClick={handleSearch}>
+          Search!
+        </button>
+      </form>
+      {!books ? (
+        <h1>Search for a book!</h1>
+      ) : (
+        books.map((book) => (
+          <div className="card" key={book.id}>
+            <h2>{book.volumeInfo.title}</h2>
+            <h5>
+              Written by:{' '}
+              {book.volumeInfo.authors.length > 1 ? (
+                book.volumeInfo.authors.map((author) => <span>{author} </span>)
+              ) : (
+                <span>{book.volumeInfo.authors[0]}</span>
+              )}
+            </h5>
+            <img src={book.volumeInfo.imageLinks.smallThumbnail} />
+            <p>{book.volumeInfo.description}</p>
+          </div>
+        ))
+      )}
     </div>
   );
 }
